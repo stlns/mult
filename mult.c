c67414dcea3e4e8792698d7e09c9b05d2f706f6f
Ash <114858615+stlns@users.noreply.github.com>
2022-10-24 11:28:35 +0400

    Add files via upload

--- mult.c
+++ mult.c
@@ -15,6 +15,9 @@ void lex(char *file) {
 				case '(': case ')': case '{': case '}': case '[': case ']': case ',': case ';': case '=': case ':': case '"': printf("yield = %c \n", ch); continue;
 				case '+': case '-': case '*': case '/': case '\\': printf("oper %c\n", ch); continue;
 				case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': printf("num = %c\n", ch); continue;
+				case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z': case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z': {
+					printf("char = %c\n", ch); continue;
+				}
 				default: printf("? = %c\n", ch); continue;
 			}
 		}
@@ -23,16 +26,13 @@ void lex(char *file) {
 }
 main(int argc, char** argw) {
 	if(argc > 1) {
+		char *file = argw[1];
+		lex(file);
 		if(argc > 2) {
-			for(int i = 1; i < argc; i++) {
-				if(ranged(0, 72, atoi(argw[i])))
-					printf("in range = %s\n", argw[i]);
-				else
-					printf("%s\n", argw[i]);
+			printf("---\n");
+			for(int i = 2; i < argc; i++) {
+				printf("%s\n", argw[i]);
 			}
-		} else {
-			char *file = argw[1];
-			lex(file);
 		}
 	} else { printf("mult!\n"); }
 	return 0;
